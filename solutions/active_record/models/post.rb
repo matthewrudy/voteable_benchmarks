32309aadeb179d35209b79f46386fe97939feaa7
Matthew Rudy Jacobs <MatthewRudyJacobs@gmail.com>
2011-05-05 17:16:29 +0800

    this seems like a good attempt although the "validates" expects it to return the record?

--- solutions/active_record/models/post.rb
+++ solutions/active_record/models/post.rb
@@ -2,20 +2,22 @@ class Post < ActiveRecord::Base
   class << self
     def vote(user_id, post_id, value)
       # create a vote if one doesnt already exist
-      connection.execute "INSERT IGNORE INTO votes SET user_id=#{user_id}, post_id=#{post_id}, value=#{Vote.connection.quote(value)}"
-    
-      reset_vote_counts(post_id)
-    end
-  
-    def reset_vote_counts(post_id)
-      # set the vote counts by summing over existing votes
-      connection.execute "UPDATE posts, votes SET votes_count=COUNT(votes.id), votes_point=SUM(IF(votes.value, 1, -2)), up_votes_count=SUM(IF(votes.value,1,0)), down_votes_count=SUM(IF(votes.value,0,1)) WHERE posts.id=#{post_id} && votes.post_id = posts.id"
+      if connection.insert("INSERT IGNORE INTO votes SET user_id=#{user_id}, post_id=#{post_id}, value=#{Vote.connection.quote(value)}")
+        point_difference = value ? "+1" : "-2"
+        direction_counter = value ? "up_votes_count" : "down_votes_count"
+
+        connection.update "UPDATE posts, votes SET votes_count=votes_count+1, votes_point=votes_point#{point_difference}, #{direction_counter}=#{direction_counter}+1 WHERE posts.id=#{post_id}"
+      end
     end
   
     def unvote(user_id, post_id)
       # create a vote if one doesnt already exist
-      if Vote.delete_all("post_id = #{post_id} AND user_id=#{user_id}") > 0
-        reset_vote_counts(post_id)
+      if vote = Vote.find_by_post_id_and_user_id(post_id, user_id)
+        vote.destroy
+        point_difference = value ? "-1" : "+2"
+        direction_counter = value ? "up_votes_count" : "down_votes_count"
+        
+        connection.update "UPDATE posts, votes SET votes_count=votes_count-1, votes_point=votes_point#{point_difference}, #{direction_counter}=#{direction_counter}-1 WHERE posts.id=#{post_id}"
       end
     end
   end
