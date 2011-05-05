1cced9a90e334a3078ec26cf2a970a4cb8a99313
Matthew Rudy Jacobs <MatthewRudyJacobs@gmail.com>
2011-05-05 17:34:38 +0800

    we dont have to return the record

--- solutions/shared/abstract_solution.rb
+++ solutions/shared/abstract_solution.rb
@@ -20,7 +20,7 @@ module AbstractSolution
     @users = (1..TOTAL_USERS).map do |i|
       User.create(
         :name => 'Alex Nguyen',
-        :happy => rand > 0.5
+        :happy => i%2 ==0
       )
     end
 
@@ -37,11 +37,10 @@ module AbstractSolution
     @users.each do |user|
       @posts.each do |post|
         # Happy user will give positive vote
-        updated_post = vote(user.id, post.id, user.happy)
-        validates(:vote, user, post, updated_post)
-        post.attributes = updated_post.attributes
+        vote(user.id, post.id, user.happy)
       end
     end
+    verify_all_votes!
   end
 
 
@@ -49,24 +48,37 @@ module AbstractSolution
     @users.each do |user|
       @posts.each do |post|
         updated_post = unvote(user.id, post.id, user.happy)
-        validates(:unvote, user, post, updated_post)
-        post.attributes = updated_post.attributes
       end
+    end
+    verify_no_votes!
+  end
+
+  def verify_all_votes!
+    @posts.each do |post|
+      post.reload
+      happy_users   = TOTAL_USERS / 2
+      unhappy_users = TOTAL_USERS - happy_users
+      
+      assert_equal TOTAL_USERS, post.votes_count, "post vote count"
+      assert_equal happy_users-(unhappy_users*2), post.votes_point, "post vote points"
+      assert_equal happy_users, post.up_votes_count, "post up votes"
+      assert_equal unhappy_users, post.down_votes_count, "post down votes"
     end
   end
 
-
-  def validates(action, user, post, updated_post)
-    direction = { 
-      :vote => 1,
-      :unvote => -1
-    }[action]
-    
-    unless post.votes_point == updated_post.votes_point - direction*VOTE_POINT[user.happy] &&
-           post.votes_count == updated_post.votes_count - direction
-      puts "#{action} ERROR"
-      # puts user.inspect, post.inspect, updated_post.inspect # DEBUG
-      throw StandardError
+  def verify_no_votes!
+    @posts.each do |post|
+      post.reload
+      assert_equal 0, post.votes_count, "post vote count"
+      assert_equal 0, post.votes_point, "post vote points"
+      assert_equal 0, post.up_votes_count, "post up votes"
+      assert_equal 0, post.down_votes_count, "post down votes"
+    end
+  end
+  
+  def assert_equal(expected, actual, message=nil)
+    if expected != actual
+      raise "expected #{expected} but got #{actual}\n#{message}"
     end
   end
     
